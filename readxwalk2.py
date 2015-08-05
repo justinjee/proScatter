1da8e1117c966e194356412e922cc42997380e74
justinjee <justinjee@gmail.com>
2015-08-05 10:42:27 -0400

    updated readxwalk2.py Can now fully convert xwalk files to pLink-ish files

--- readxwalk2.py
+++ readxwalk2.py
@@ -1,6 +1,10 @@
 #fasta
 import re
 import sys
+from collections import defaultdict
+
+#first argument: fasta filename, 2nd argument: xwalk filename, 3rd argument: a.a. of interest
+#EXAMPLE: python readxwalk2.py ECRNINVIVO1.fasta uvrdrnap.txt K > output.txt
 
 def minGreaterThan(ar,n):
     m = max(ar)
@@ -8,8 +12,6 @@ def minGreaterThan(ar,n):
         if i<m and i>n:
             m = i
     return m
-
-#first argument: fasta filename, 2nd argument: xwalk filename, 3rd argument: a.a. of interest
 
 f = open(sys.argv[1],'r')
 #function to open and read a fasta file. remember to press i
@@ -54,7 +56,7 @@ for line in f:
     xd[temp2[2]].add(int(temp2[1]))
  
 #compare
-#x2prot = {} #xlink label (A or B): 
+x2prot = defaultdict(list) #xlink label (A or B): sorted array of (end coordiate, protein name) 
 for k in xd:
     s1 = xd[k]
     totalscore = 0
@@ -73,7 +75,8 @@ for k in xd:
                     maxscore = score
                     maxname = k2
                     maxoffset = i
-        print k + " part "+str(part)+" is " + maxname + " at "+ str(maxoffset)
+        #print k + " part "+str(part)+" is " + maxname + " at "+ str(maxoffset)
+        x2prot[k].append((maxoffset,maxname))
         totalscore += maxscore
         startoffset = maxoffset+ld[maxname]
         stopoffset = minGreaterThan(s1,startoffset)
@@ -81,3 +84,20 @@ for k in xd:
 
 #write new plink file
 
+def translate(name,loc,x2prot):
+    ar = x2prot[name]
+    for i in range(len(ar)):
+        if i+1>=len(ar) or loc<ar[i+1][0]:
+            newname=ar[i][1]
+            newloc=loc-ar[i][0]
+            break
+    return newname+"("+str(newloc)+")"
+
+f = open(sys.argv[2],'r')
+for line in f:
+    temparray = line.split()
+    temp1 = temparray[2].split('-')
+    temp2 = temparray[3].split('-')
+    (loc1,name1) = temp1[1:3]
+    (loc2,name2) = temp2[1:3]
+    print ', # # # # 1 # # # # # # # '+translate(name1,int(loc1),x2prot)+'-'+translate(name2,int(loc2),x2prot)
