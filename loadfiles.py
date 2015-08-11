0283d95c8f9759f3ffeefd770f74059b43596e5d
Ilya Shamovsky <ilya.shamovsky@gmail.com>
2015-08-11 14:37:14 -0400

    Refactor load_fasta, start work on load_plink

--- loadfiles.py
+++ loadfiles.py
@@ -1,11 +1,21 @@
 from collections import defaultdict
+from skbio.parse.sequences import parse_fasta
 import os
 import re
 import numpy as np
 
-def loadfasta(fasta,aa):
-#input: name of fasta file
-#create map of ordered amino acids of interest
+def loadfasta(fasta, aa):
+    '''
+    input: name of fasta file
+    create map of ordered amino acids of interest
+    '''
+    prot2map = {}
+    for name, seq in parse_fasta(fasta):
+        prot2map[name] = defaultdict(int)
+        prot2map[name].update({i:i for i,c in enumerate(seq) if c in aa})
+    return prot2map
+
+def ldfasta(fasta, aa):
     prot2map = {}
     prot = ''
     i=0
@@ -18,55 +28,58 @@ def loadfasta(fasta,aa):
             j=1
         else:
             for c in line.rstrip():
-              if not c.isspace():
-                if c in aa:
-                    prot2map[prot][i]=j
+                if not c.isspace():
+                    if c in aa:
+                        prot2map[prot][i]=j
                     j+=1
                 i+=1
     return prot2map
 
 
-def loadplink(dir, prot2map, allprot, scale, cutoff=1):
-#input: directory full of plink txt files, the prot2map from loadfasta, and the dictionary of all involved proteins thus far
-#creates a dictionary of gene-gene to coordinate tuples and updates allprot
- gg2i = {} #gene-gene to interaction coordinates
+def loadplink(directory, prot2map, allprot, scale, cutoff=1):
+    '''
+    Input: directory full of plink txt files, the prot2map from loadfasta,
+    and the dictionary of all involved proteins thus far creates a dictionary of 
+    gene-gene to coordinate tuples and updates allprot
+    '''
+    gg2i = {} #gene-gene to interaction coordinates
 
- for filename in os.listdir(dir):
-  if filename.endswith(".txt"):
-    f = open(dir+'/'+filename,'r')
-    for line in f:
-        if (',' in line) and ')-' in line and not ('REVERSE' in line):
-            tokens = (line.rstrip()).split()
-            interaction = tokens[-1]
-            # optional e value cutoff with 2.0E-04 as example
-            e = float(tokens[5])
-            if e < cutoff:
-              subtokens = re.split(r'[)|(|-]',interaction)
-              protloc = [(subtokens[0],subtokens[1]),(subtokens[3],subtokens[4])]
-              protloc = sorted(protloc)
-              prot1 = protloc[0][0]
-              prot2 = protloc[1][0]
-              if prot1 in prot2map and prot2 in prot2map:
-                loc1 = prot2map[prot1][int(protloc[0][1])]
-                loc2 = prot2map[prot2][int(protloc[1][1])]
-                if scale:
-                    loc1 = int(protloc[0][1])
-                    loc2 = int(protloc[1][1])
-                key = prot1 + '-' + prot2
-                if not key in gg2i:
-                    gg2i[key] = defaultdict(int)
-                gg2i[key][(loc1,loc2)] += 1
+    filenames = [fname for fname in os.listdir(directory) if fname.endswith('.txt')]
+    for filename in filenames:
+        with open(os.path.join(directory, filename), 'r') as fi:
+            for line in fi:
+                if (',' in line) and ')-' in line and not ('REVERSE' in line):
+                    tokens = (line.rstrip()).split()
+                    interaction = tokens[-1]
+                    # optional e value cutoff with 2.0E-04 as example
+                    e = float(tokens[5])
+                    if e < cutoff:
+                        subtokens = re.split(r'[)|(|-]',interaction)
+                    protloc = [(subtokens[0],subtokens[1]),(subtokens[3],subtokens[4])]
+                    protloc = sorted(protloc)
+                    prot1 = protloc[0][0]
+                    prot2 = protloc[1][0]
+                    if prot1 in prot2map and prot2 in prot2map:
+                        loc1 = prot2map[prot1][int(protloc[0][1])]
+                        loc2 = prot2map[prot2][int(protloc[1][1])]
+                        if scale:
+                            loc1 = int(protloc[0][1])
+                            loc2 = int(protloc[1][1])
+                        key = prot1 + '-' + prot2
+                        if not key in gg2i:
+                            gg2i[key] = defaultdict(int)
+                        gg2i[key][(loc1,loc2)] += 1
 
-                #Now handle symmetry
-                key = prot2 + '-' + prot1
-                if not key in gg2i:
-                    gg2i[key] = defaultdict(int)
-                gg2i[key][(loc2,loc1)] += 1
+                        #Now handle symmetry
+                        key = prot2 + '-' + prot1
+                        if not key in gg2i:
+                            gg2i[key] = defaultdict(int)
+                        gg2i[key][(loc2,loc1)] += 1
 
-                #Add protein names to allprot for graphing purposes
-                allprot[prot1]=max(prot2map[prot1])
-                allprot[prot2]=max(prot2map[prot2])
- return (gg2i,allprot)
+                        #Add protein names to allprot for graphing purposes
+                        allprot[prot1]=max(prot2map[prot1])
+                        allprot[prot2]=max(prot2map[prot2])
+    return (gg2i,allprot)
 
 
 def writesummary(key,gg2i,prot2map,scale):
